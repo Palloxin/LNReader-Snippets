ac1685684c127a581595b02fbdd387d29863b572
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-20 15:30:51 +0200

    V3

--- JS/Time Format Conversion/12h→24h time format.js
+++ JS/Time Format Conversion/12h→24h time format.js
@@ -1,21 +1,21 @@
 /////↓↓↓↓↓↓↓↓↓↓ 12h→24h time format
-chapter = document.querySelector('#LNReader-chapter');
+chapter = document.querySelector('chapter');
 chapter.innerHTML = chapter.innerHTML
+.replace(/\b(?<!:|\d-)(?=(?:[1-9]|1[012]|0\d(?=:))\b(?::[0-5]\d\b)?(?:-| ?[AaPp]\.?[mM](?=\.[^\.]|\b)))/g, '••••')//♦️
 //↓ the . of ``a/pm.`` ends a phrase and continues
-.replace(/\b(?<!\:|\d-)((?:[1-9]|10|11|12|\d\d?:\d\d) ?[ap])\.?m(?=\.[^\.]|\b)/gi, (_, a)=> `•••${a.toLowerCase()}m.`)//♦
+.replace(/••••[\d:\s]+[ap])\.?m(?=\.[^\.]|\b)/gi, (_, a)=> `••••${a.toLowerCase()}m.`)
 //↓ cases like 3-4 pm
-.replace(/\b(?<!\:)([1-9]|10|11)-([1-9]|10|11|12)\s*([ap]\.?m\b)/gi, '•••$1 $3°°-°°•••$2 $3.')
+.replace(/••••((?!12)\d+)-(\d+)\s*([ap]\.?m\b)/gi, '••••$1 $3°°-°°••••$2 $3.')
 //↓ \d AM/PM indicates a different acronym
-.replace(/•••(?<=\bwith •••)(\d+ [ap])\.?m(?:\.(?!\.)|\b)/g, (_, a) => `${a.toUpperCase()}M`)
+.replace(/••••(?<=\bwith ••••)(\d+ [ap])\.?m(?:\.(?!\.)|\b)/g, (_, a) => `${a.toUpperCase()}M`)
 //↓ exception
-.replace(/•••12(:\d\d)?\s*([ap])m(?:\.|\b)/g, (_, z,i)=>{
-	let gag = z;
-	if(z === undefined) gag = ":00";
-	if(i ==="a") return `00${gag}`; 
-	else return `12${gag}`;
+.replace(/••••12(:\d\d)?\s*([ap])m(?:\.|\b)/g, (_, z,i)=>{
+let gag = z || ":00";
+if(i ==="a") return `00${gag}`;
+else return `12${gag}`;
 })
 
-.replace(/•••(\d+)(?::(\d\d))?\s*([ap])m(?:\.|\b)/g, (_, a,b,c)=>{
+.replace(/••••(\d+)\s*(?::\s*(\d\d))?\s*([ap])m(?:\.|\b)/g, (_, a,b,c)=>{
     const h = +a;
     const m = +b || 0;
     if(c === "a")
@@ -23,9 +23,10 @@ chapter.innerHTML = chapter.innerHTML
     else
       return `${(h+12)%24}:${`${m}`.padStart(2,"0")}`;
 })
+
 .replace(/\b([2-9]|10|11) (o’clock at night|in the (?:evening|afternoon))\b/g, (_, a,b)=> `${Math.round(+a + 12)}:00 ${b.replace(/o’clock /, '')}`)
-.replace(/(?<=\:\d\d|\b[AP]M)\.(?=[\.\,])/g, '')
-.replace(/°°-°°/g, ' - ')//sixth spaces
-.replace(/•••(?=\d)/g, '')
+.replace(/(?<=:\d\d|\b[AP]M)\.(?=[\.\,])/g, '')
+.replace(/°°\-°°/g, '-')//sixth spaces
+.replace(/••••(?=\d)/g, '')
 ;
 /////↑↑↑↑↑↑↑↑↑↑ – END
