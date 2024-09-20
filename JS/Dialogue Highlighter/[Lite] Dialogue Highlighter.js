4dd2dc74778b0d565762d75d6e5784d1e1507ec6
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-20 12:57:25 +0200

    .

---
+++ JS/Dialogue Highlighter/[Lite] Dialogue Highlighter.js
@@ -0,0 +1,29 @@
+//Cleaning of quotation marks contexts
+////↓↓↓↓↓ — 
+chapter = document.querySelector('chapter');
+chapter.innerHTML = chapter.innerHTML
+.replace(/(?:‘|’(?<=\W’)(?!s?\s))([^\"”“\'’‘\<]+)(?:(?<!\s)‘|’(?![a-z]))/g, '‘$1’')//test-strings: ``Can’t u do the ’job’?``|||``‘He said ‘something’!’``|||``‘We don’t!’ They said on the Merfolk Pirates’ deck.``|||
+.replace(/”(?<=(?:<p>|\: ?)”)/g, '“')
+.replace(/“(?=<\/p>)/g, '”')
+.replace(/’(?<=<p>’)/g, '‘')
+.replace(/‘(?=<\/p>)/g, '’')
+.replace(/’(?=\w\w\w+)(?<![\s\w]’)/g, '’ ')
+.replace(/(?: ([\”’])|([\“‘]) )/g, '$1$2')
+
+///////↓↓↓↓ — Dialogue highlighter
+const regex = /[\"“”](?<!\=\")(?!>|\s?[\"“”])([^\"“”]+?)(<br>[^\"“”]+)?[\"”](?<!=\")/g;
+const colorElement = (x) => {
+    x.innerHTML = x.innerHTML.replace(regex, 
+			`"<span style="color: #FFFFEB;">$1$2</span>"`
+	);
+};
+(col = (parent) => {
+    if (parent.nodeName === "SCRIPT") return;
+    const childsWith = [...parent.children].filter((ch) =>
+        /[\"“”]/.exec(ch.innerText)
+    );
+    if (!childsWith.length) colorElement(parent);
+    else childsWith.forEach((ch) => col(ch));
+})(document.querySelector("chapter"));
+///////↑↑↑↑ — END
+//LITE version manipulates less the text
