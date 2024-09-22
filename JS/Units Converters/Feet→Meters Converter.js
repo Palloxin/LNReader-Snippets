40fd182221f4459195a21cf6b343535dd7fa803f
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-22 14:56:47 +0200

    ;;

--- JS/Units Converters/Feet→Meters Converter.js
+++ JS/Units Converters/Feet→Meters Converter.js
@@ -14,7 +14,7 @@ chapter.innerHTML = chapter.innerHTML
 	let unRounded = 4 / fff;
 	let nnn = +gioco.indexOf(a) * fff * multip;
 	if(/\d/.test(a)) nnn =  +a * fff * multip;
-	if(c) {nnn = nnn * -1; a = a + b};
+	if(c) {nnn = nnn * -1; a = a + b;}
 	if(b === " and a half") nnn = nnn + (0.5 * fff);
 	if(nnn) nnn = nnn.toFixed(2);
 	if(nnn > unRounded) nnn = Math.round(nnn);
