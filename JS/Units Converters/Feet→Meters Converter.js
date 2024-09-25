2131629039415f7dc1324d130c1d23f971c2412c
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-26 01:12:41 +0200

    .

--- JS/Units Converters/Feet→Meters Converter.js
+++ JS/Units Converters/Feet→Meters Converter.js
@@ -30,5 +30,5 @@ chapter.innerHTML = chapter.innerHTML
     
 
 //not work strings: ||a long body of over a hundred feet||six to eight feet tall||the horn grows by ten feet||
-//jsfidle: https://jsfiddle.net/wjmekrL7/1/
+//jsfidle: https://jsfiddle.net/wjmekrL7/2/
 //////↑↑↑↑↑ — END
