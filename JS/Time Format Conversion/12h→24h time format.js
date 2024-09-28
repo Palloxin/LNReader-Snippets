54eb72ecd6a6b8bbb125176cc7edf73f4d8480ae
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-28 16:07:54 +0200

    faster

--- JS/Time Format Conversion/12h→24h time format.js
+++ JS/Time Format Conversion/12h→24h time format.js
@@ -25,9 +25,9 @@ else return `12${gag}`;
 })
 
 //↓ o’clock
-.replace(/’(?<=(\d+)? \bo’) ?clock/g, '★$1')
-.replace(/★(?<=([a-z]+|\d+) o★)/g, (_, a) => `’clock${{two:2, three:3, four:4, five: 5, six:6, seven:7, eight:8, nine:9, ten:10, eleven:11}[a]||''}`)
-.replace(/’clock(\d+)( at(?<=\D(?:[7-9]|1[01]) at) night| in(?<=[2-9] in) the afternoon)?\b/g, (_, a,b)=> `’clock ${b ? '(' + (+a+12) + ':00\)' : ''}${b||''}`)
+.replace(/’(?<=(\b\d+)? \bo’) ?clock/g, '★$1')
+.replace(/★(?<=([a-z\d]+) o★)/g, (_, a) => `’clock${{two:2, three:3, four:4, five: 5, six:6, seven:7, eight:8, nine:9, ten:10, eleven:11}[a]||''}`)
+.replace(/’clock(\d+)( at(?<=\D(?:[7-9]|1[01]) at) night| in(?<=[1-9] in) the afternoon)?\b/g, (_, a,b)=> `’clock${b||''} ${b ? '(' + (+a+12) + ':00\)' : ''}`)
 //↑
 .replace(/(:\d\d|\b[AP]M)\.(?=[\.\,])/g, '$1')
 .replace(/°°-°°/g, ' - ')//sixth spaces
