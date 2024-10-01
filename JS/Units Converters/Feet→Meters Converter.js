83e4f768e7bf725efc1f30bcd888d40909287ebb
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-10-01 02:41:14 +0200

    .

--- JS/Units Converters/Feet→Meters Converter.js
+++ JS/Units Converters/Feet→Meters Converter.js
@@ -10,7 +10,7 @@ chapter.innerHTML = chapter.innerHTML
 .replace(/™™™feet²? (?=tall|thick|long|wide|lower|radius|distance|away|from|in (?:length|height|diameter)|(?:deep|high)(?! in(to)?\b))/g, '⋮⋮⋮feet ')
 .replace(/™™™(?<=(?:(?:height|altitude|length|width|wingspan|range) of (?:almost|over|about)? ?|as long as )(?=[otfsen\d])(?:[a-z\d]+|[a-z]+\s[a-z]+) ™+)feet/g, '⋮⋮⋮feet')
 .replace(/⋮⋮⋮feet(?<=\b((?<![efhnrx]ty-)(?:two|three|four|five|six|seven|eight|nine)|(?:twen|thir|for|fif|six|seven|eigh|nine)ty(?:\-(?:one|two|three|four|five|six|seven|eight|nine))?|(?:thir|four|fif|six|seven|eigh|nine)teen|ten|eleven|twelve|\d+(?:\.\d+)?|(?:a|one)(?! [a⋮]))( and a half| (hundred|thousand))? ⋮⋮⋮feet)(²)?/g, (_, a,b,c,d) => {
-	const mi = {hundred:100,thousand:1000}[c] || 1;
+	const mi = {hundred:100, thousand:1000}[c] || 1;
 	let ff = 0.305;
 	if(d) ff = ff **2;
 	let nnn = /\d/.test(a) ? +a*ff*mi : +tico.indexOf(a)*ff*mi;
