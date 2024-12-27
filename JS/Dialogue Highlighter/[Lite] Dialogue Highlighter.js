5dbc2c8d1e6d3f6bca72cd6c66be94e5d15d402a
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-12-27 01:18:41 +0100

    Update [Lite] Dialogue Highlighter.js

--- JS/Dialogue Highlighter/[Lite] Dialogue Highlighter.js
+++ JS/Dialogue Highlighter/[Lite] Dialogue Highlighter.js
@@ -12,9 +12,11 @@ chapter.innerHTML = chapter.innerHTML
 ///////↓↓↓↓ — Dialogue highlighter
 const regex = /[\"“”](?<!\=\")(?!>|\s?[\"“”])([^\"“”]+?)(<br>[^\"“”]+)?[\"”](?<!=\")/g;
 const colorElement = (x) => {
-    x.innerHTML = x.innerHTML.replace(regex, 
-			`"<span style="color: #FFFFEB;">$1$2</span>"`
-	);
+    x.innerHTML = x.innerHTML
+	.replace(/=\"/g, '=\'')
+        .replace(/\">/g, '\'>')
+	.replace(regex, `"<span style="color: #FFFFEB;">$1$2</span>"`)
+	.replace(/[\u2033]/g, '\"');
 };
 (col = (parent) => {
     if (parent.nodeName === "SCRIPT") return;
@@ -23,6 +25,5 @@ const colorElement = (x) => {
     );
     if (!childsWith.length) colorElement(parent);
     else childsWith.forEach((ch) => col(ch));
-})(document.querySelector("chapter"));
-///////↑↑↑↑ — END
-//LITE version manipulates less the text
+})(document.querySelector('#LNReader-chapter'));
+//////↑↑↑↑ — END
