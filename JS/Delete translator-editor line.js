d44495c2ef7690e9a79390cc0dcee2da4e4a02ef
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-03-11 21:34:26 +0100

    Update row 23

--- JS/Delete translator-editor line.js
+++ JS/Delete translator-editor line.js
@@ -1,3 +1,5 @@
 chapter = document.querySelector('#LNReader-chapter');
 chapter.innerHTML = chapter.innerHTML
-.replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '');
+.replace(/&nbsp;/g, '\u00a0')
+.replace(/<p>\s*<\/p>/g, '')
+.replace(/^(?:.*?<p>.+?<\/p>){4}/, (_) => `${_.replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '')}`)
