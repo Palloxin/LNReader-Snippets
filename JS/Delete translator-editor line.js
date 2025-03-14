d6e8ca0560d5df6d04f4a70f7c493cb58e85a816
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-03-14 11:43:47 +0100

    Update Delete translator-editor line.js

--- JS/Delete translator-editor line.js
+++ JS/Delete translator-editor line.js
@@ -2,4 +2,4 @@ chapter = document.querySelector('#LNReader-chapter');
 chapter.innerHTML = chapter.innerHTML
 .replace(/&nbsp;/g, '\u00a0')
 .replace(/<p>\s*<\/p>/g, '')
-.replace(/^(?:.*?<p>.+?<\/p>){4}/, (_) => `${_.replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '')}`)
+.replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '')
