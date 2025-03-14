20928349fe01865ebde9b7e48dd06053a42db7de
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-03-14 17:04:47 +0100

    Update Universal-Cleaner [EN].js

--- JS/Universal-Cleaner/Universal-Cleaner [EN].js
+++ JS/Universal-Cleaner/Universal-Cleaner [EN].js
@@ -207,7 +207,7 @@ imgs.push(Y); return "䷢䷢䷢"+imgs.length;})
 //↓ misc
 .replace(/\/p>(?=[^<♪]+<)/g, '/p><p>')//give p to tagless
 .replace(/-(?<!<[^>]+-)(?![^<]+>)(?<=\b\w\w?\w?-)(?=\w)/g, '-⁠')//u2060
-.replace(/\.(?<=\b(?:M[sr]s?|etc)\.) /g, '<span style="font-size: 0.8em;">.</span> ')
+.replace(/\.(?<=\b(?:M[sr]s?|etc)\.)(?=[ ,])/g, '<span style="font-size: 0.8em;">.</span>')
 //↑
 //↓↓↓↓↓↓↓ thousands separator— n ≤9999 excluded—
 .replace(/,(?=\d\d\d\D)/g, '±±')
