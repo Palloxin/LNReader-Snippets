e86bd75641c37684ee84f9916e7c99a9c1579b97
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-03-24 12:19:53 +0100

    Update 12h→24h time format.js

--- JS/Time Format Conversion/12h→24h time format.js
+++ JS/Time Format Conversion/12h→24h time format.js
@@ -1,17 +1,17 @@
 /////↓↓↓↓↓↓↓↓↓↓ 12h→24h time format
 chapter = document.querySelector('#LNReader-chapter');
 chapter.innerHTML = chapter.innerHTML
-.replace(/\b(?<!:|\d-)(?:[1-9]|1[012]|0\d(?=:\d))(?::[0-5]\d)?(?:-| ?[AaPp]\.?[mM](?=\.[^\.]|\b))/g, '••••$&')//♦
+.replace(/\b(?<!:|\d-)(?:[1-9]|1[012]|0\d(?=[:\.]\d))(?:[\.:][0-5]\d)?(?:-| ?[AaPp]\.?[mM](?=\.[^\.]|\b))/g, '••••$&')//♦
 //↓ the . of ``a/pm.`` ends a phrase and continues
-.replace(/••••([\d:\s]+[ap])\.?m\b/gi, (_, a)=> `••••${a.toLowerCase()}m.`)
+.replace(/••••([\d:\s\.]+[ap])\.?m\b/gi, (_, a)=> `••••${a.toLowerCase()}m.`)
 //↓ cases like 3-4 pm
 .replace(/••••((?!12)\d+)-(\d+)\s*([ap]\.?m\b)/gi, '••••$1 $3°°-°°••••$2 $3.')
 //↓ \d AM/PM indicates a different acronym
 .replace(/••••(?<=\bwith •+)(\d+ [ap])\.?m\b/g, (_, a) => `${a.toUpperCase()}M`)
 //↓ exception
-.replace(/••••12(:\d+)?\s*([ap])m(?:\.|\b)/g, (_, z,i)=> `${i==='a'?'00':'12'}${z||':00'}`)
+.replace(/••••12([:\.]\d+)?\s*([ap])\.?m(?:\.|\b)/g, (_, z,i)=> `${i==='a'?'00':'12'}${z||':00'}`)
 
-.replace(/••••(\d+)(?::(\d\d))?\s*([ap])m(?:\.|\b)/g, (_, a,b,c)=>{
+.replace(/••••(\d+)(?:[:\.](\d\d))?\s*([ap])m(?:\.|\b)/g, (_, a,b,c)=>{
     const h = +a;
     const m = +b || 0;
     if(c === "a")
@@ -28,5 +28,5 @@ chapter.innerHTML = chapter.innerHTML
 .replace(/°°-°°/g, ' - ')//sixth spaces
 .replace(/••••(?=\d)/g, '')
 ;
-//fiddle: https://jsfiddle.net/a2z3omye/
+//fiddle: https://jsfiddle.net/rndoah4v/
 /////↑↑↑↑↑↑↑↑↑↑ – END
