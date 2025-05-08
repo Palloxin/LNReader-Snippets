0717d0ec77fd09ebc395592f9d7b351252224b35
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-05-08 11:47:44 +0200

    Update Word-Character Counter.js

--- JS/Word-Character Counter.js
+++ JS/Word-Character Counter.js
@@ -1,6 +1,6 @@
 //↓↓↓
 chapter = document.querySelector('#LNReader-chapter');
-var inerText = chapter.innerText;
-var counttt = inerText.length - 10 - reader.chapter.name.length;
+var inerText = chapter.innerHTML.replace(/&nbsp;/g, ' ').replace(/<[^>]+>/g, '').replace(/&(?:lt|gt|amp);/g, '').replace(/\s\s+/g, ' ');
+var counttt = inerText.match(/\S+/g).length;
 chapter.innerHTML = `<b style="position:absolute; right:0.7em;top:-0.21em;font-size:8px;">${counttt}</b>${chapter.innerHTML}`;
 //↑↑↑ 
