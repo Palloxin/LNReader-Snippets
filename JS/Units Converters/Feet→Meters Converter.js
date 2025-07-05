6c692b890010d0a420caa1229e6f46636aa6efaa
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-07-05 21:26:40 +0200

    Update row 30 (#30)
    * Update row 30

--- JS/Units Converters/Feet→Meters Converter.js
+++ JS/Units Converters/Feet→Meters Converter.js
@@ -7,8 +7,8 @@ chapter = document.querySelector('#LNReader-chapter');
 chapter.innerHTML = chapter.innerHTML
 .replace(/\b(square )?f(ee|oo)t\b/g, (_, i,l) => `™™™f${l==='ee'?'%%':'%'}t${i?'²':''}`)
 .replace(/,(?=\d\d\d(?:\,|\d\d\d)* ™™™f%+t)/g, '')
-.replace(/™™™f(%+t²?) (?=tall|thick|long|wide|height|lower|radius|distance|away|from|in (?:length|height|diameter)|(?:deep|high)(?! in(?:to)?\b))/g, '⋮⋮⋮f$1 √√')
-.replace(/™™™(?<=(?:(?:height|altitude|length|width|wingspan|range) of (?:almost|over|about|approximately)? ?|as long as | the (?=[^™⋮<]+™+f%t))(?=[otfsen\d])(?:[a-z\d]+|[a-z]+\s[a-z]+) ™™™)(?=f%+t)/g, '⋮⋮⋮')
+.replace(/™™™f(%+t²?) (?=tall|thick|long|wide|height|lower|radius|distance|away|from|in (?:length|height|diameter)|(?:deep|high)(?! in(?:to)?\b)|of distance)/g, '⋮⋮⋮f$1 √√')
+.replace(/™™™(?<=(?:\b(?:height|altitude|length|width|wingspan|range) of (?:almost|over|about|approximately)? ?|within |as long as | the (?=[^™⋮<]+™+f%t))(?=[otfsen\d])(?:[a-z\d]+|[a-z]+\s[a-z]+) ™™™)(?=f%+t)/g, '⋮⋮⋮')
 .replace(/⋮⋮⋮f(%+)t(?<=\b((?:twen|thir|for|fif|six|seven|eigh|nine)ty(?:-(?:one|two|three|four|five|six|seven|eight|nine))?|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|(?:thir|four|fif|six|seven|eigh|nine)teen|\d+(?:\.\d+)?|(?:a|one)(?! [a⋮]))( and a half| (hundred|thousand))? ⋮⋮⋮f%+t)(²)?/g, (_,z, a,b,c,d) => {
 	const mi = {hundred:100, thousand:1000}[c] || 1;
 	let ff = 0.305;
@@ -20,11 +20,11 @@ chapter.innerHTML = chapter.innerHTML
 	if(nnn > 11) nnn = Math.round(nnn);
 	return `⋮⋮⋮f${z}t${d||''} ÷×(${nnn}m${d||''})`})
 
-.replace(/÷×(\([\d\.]+m²?\)) √√((?:in )?[a-z]+)/g, '$2 $1')
+.replace(/÷×(\([\d\.]+m²?\)) √√((?:in |of (?=distance))?[a-z]+)/g, '$2 $1')
 .replace(/÷×/g, '')
 .replace(/(?:⋮⋮⋮|™™™)f(%+)t(²)?(?: √√)?\s*/g, (_,z, a) => `${a?'square ':''}f${z==='%'?'oo':'ee'}t `)
 
-.replace(/(\d)[\'’](?<=\s\d.)(\d(?!\d))\"?/g, (_, a,b) => {
+.replace(/(\d)[\'’](?<=\s\d.)(\d(?!\d))\"?(?: ft\b)?/g, (_, a,b) => {
 	let feeinc = (+a * 0.305) + (+b * 0.0254);
 	return ` ${feeinc.toFixed(2)}m`})
 
