071b0f096cf4a82b200e9311fbeddf8cd7466f40
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-09-03 23:35:56 +0200

    Update Universal-Cleaner [EN].js

--- JS/Universal-Cleaner/Universal-Cleaner [EN].js
+++ JS/Universal-Cleaner/Universal-Cleaner [EN].js
@@ -96,8 +96,8 @@ imgs.push(Y); return "䷢䷢䷢"+imgs.length;})
     let t = /\'(?<!(?:[\s\W]|[^Ss]).)/g;
     return `'${t.test(a)?a.replace(t, '’').replace(/’(?<=\'[^\'’]+’)(?=[^\']+$)/, '\''):a}'`})
 .replace(/\'(?<!\<\/?p>.)(?!<\/?p>)(?<=<p>[^\']+?\')(?=[^\w\'][^\']+?<\/p>)/g, '’')//<p>[^\']+?
-// - https://jsfiddle.net/6wf8bnxr/
-//↑↑↑ - https://jsfiddle.net/69zbg81a/1/
+// - jsfiddle.net/hjgmovt9/
+//↑↑↑ - jsfiddle.net/69zbg81a/1/
 //↓
 .replace(/[”“](?=(?:[dmst]|ll|ve)\b(?!-))(?<=\w)/g, '’')
 .replace(/‘((?:[Ii]t|[Yy]ou|[Ss]?[Hh]e|[Ww]e|[Tt]hey)(?=’[lv])|(?:If )?I)’(ll|ve|m)\b/g, (l, j,i) => `‘${j} ${{'m':'am','ll':'will'}[i]||'have'}`)
@@ -244,3 +244,4 @@ imgs.push(Y); return "䷢䷢䷢"+imgs.length;})
 
 //%%%%%%%%%
 ;
+
