27fe477e6bdbfa29db16eee3947630412d461ae8
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-09-20 20:45:01 +0200

    Update Delete translator-editor line.js

--- JS/Delete translator-editor line.js
+++ JS/Delete translator-editor line.js
@@ -3,4 +3,6 @@ chapter.innerHTML = chapter.innerHTML
 .replace(/&nbsp;/g, '\u00a0')
 .replace(/<p>\s*<\/p>/g, '')
 .replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s\-]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s\-]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '')
+.replace(/[\u00a0]/g, '&nbsp;')
 // - https://jsfiddle.net/yau30t8m/
+
