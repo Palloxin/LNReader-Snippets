f6faf6cc177b3eeb9d4e98a05f9592839a71f399
Palloxin <75091899+Palloxin@users.noreply.github.com>
2025-10-15 19:14:15 +0200

    Update row 36

--- JS/Delete translator-editor line.js
+++ JS/Delete translator-editor line.js
@@ -5,4 +5,3 @@ chapter.innerHTML = chapter.innerHTML
 .replace(/<p>\s*(?:<\w+>\s*)*Translator:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s\-]+\.?)[^\w<]*(?:<\/?\w+>\s*)*Editor:[^\w<]*(?:<\/?\w+>\s*)*(?:\w[\w’\s\-]+\.?)(?:<\/?\w+>\s*)*(?<!\s*<p>)<\/p>/, '')
 .replace(/[\u00a0]/g, '&nbsp;')
 // - https://jsfiddle.net/yau30t8m/
-
